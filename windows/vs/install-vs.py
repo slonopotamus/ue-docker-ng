5aca3c8ccc0429d338946bf13833e356d8b82313
Marat Radchenko <marat@slonopotamus.org>
2026-06-19 18:17:03 +0300

    Drop pre-5.4 support

--- windows/vs/install-vs.py
+++ windows/vs/install-vs.py
@@ -19,8 +19,6 @@ vs2022_components = common_components + [
     "Microsoft.NetCore.Component.SDK",
 ]
 
-ue5_vs_fallback_version = "17.8"
-
 
 def build_vs_channel_url(vs_version: str) -> str:
     version_major = vs_version.split(".", 1)[0]
@@ -35,17 +33,14 @@ class VisualStudio:
 
     @staticmethod
     def from_json(json_config: Any):
-        # TODO: Add option to request VS2025
+        # TODO: Add option to request VS2026
 
-        try:
-            vs_version = json_config["MinimumVisualStudio2022Version"]
-        except KeyError:
-            vs_version = ue5_vs_fallback_version
+        vs_version = json_config["MinimumVisualStudio2022Version"]
 
         components = (
-            vs2022_components
-            + json_config["VisualStudioSuggestedComponents"]
-            + json_config["VisualStudio2022SuggestedComponents"]
+                vs2022_components
+                + json_config["VisualStudioSuggestedComponents"]
+                + json_config["VisualStudio2022SuggestedComponents"]
         )
 
         # UE-5.4 has buggy component version
@@ -96,48 +91,9 @@ class VisualStudio:
 
 
 if __name__ == "__main__":
-    windows_sdk_path = sys.argv[1]
-    build_version_path = sys.argv[2]
-
-    if os.path.exists(windows_sdk_path):
-        print(f"Using Windows_SDK.json to determine VS components")
-        # 5.4 or newer
-        with open(sys.argv[1]) as windows_sdk_file:
-            windows_sdk_json = jsonc.load(windows_sdk_file)
-        vs = VisualStudio.from_json(windows_sdk_json)
-    else:
-        print(f"Using fallback to determine VS components")
-        with open(build_version_path) as build_version_file:
-            engine_version = json.load(build_version_file)
-        engine_major = engine_version["MajorVersion"]
-        engine_minor = engine_version["MinorVersion"]
-
-        if engine_major == 5:
-            # Assuming 5.3 or older
-            vs = VisualStudio(
-                ue5_vs_fallback_version,
-                build_vs_channel_url(ue5_vs_fallback_version),
-                vs2022_components
-                + [
-                    "Microsoft.Net.Component.4.6.2.TargetingPack",
-                    "Microsoft.NetCore.Component.Runtime.3.1",
-                    f"Microsoft.VisualStudio.Component.VC.14.38.{ue5_vs_fallback_version}.x86.x64",
-                    "Microsoft.VisualStudio.Component.Windows10SDK.20348",
-                ],
-            )
-        else:
-            # Assuming 4.27
-            vs = VisualStudio(
-                "15",
-                f"https://aka.ms/vs/15/release/channel",
-                common_components
-                + [
-                    "Microsoft.Net.Component.4.5.TargetingPack",
-                    "Microsoft.Net.Component.4.6.2.TargetingPack",
-                    "Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
-                    "Microsoft.VisualStudio.Component.Windows10SDK.17763",
-                ],
-            )
+    with open(sys.argv[1]) as windows_sdk_file:
+        windows_sdk_json = jsonc.load(windows_sdk_file)
+    vs = VisualStudio.from_json(windows_sdk_json)
 
     vs_installer_path = "C:/vs_buildtools.exe"
     vs.download_installer(vs_installer_path)
